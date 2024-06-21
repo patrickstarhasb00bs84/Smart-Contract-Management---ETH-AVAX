37888436080794c76a4632568328eb33c988c007
Mark Revin Fragata <115454281+patrickstarhasb00bs84@users.noreply.github.com>
2024-06-22 03:24:29 +0800

    Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -7,54 +7,45 @@ contract Assessment {
     address payable public owner;
     uint256 public balance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposit(address from, uint256 amount);
+    event Withdraw(address to, uint256 amount);
+    event Transfer(address to, uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
-        balance = initBalance;
+        contractBalance = initBalance;
     }
 
     function getBalance() public view returns(uint256){
-        return balance;
+        return address(this).balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
 
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+        require(msg.value > 0, "Deposit amount should be greater than 0");
+        contractBalance += msg.value;
+        emit Depost(msg.sender, msg.value);
     }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
+        require(_withdrawAmount > 0, "Withdrawal amount should be greater than 0");
+        require(contractBalance >= _withdrawAmount, "Insufficient Contract Balance");
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
+        payable(msg.sender).transfer(_withdrawAmount);
+        contractBalance -= _withdrawAmount;
+        emit Withdraw(msg.sender, _withdrawAmount);
+    }
 
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
+    function transfer(address _to, uint256 _amount) public payable {
+        require(_to != address(0), "Invalid recipient address");
+        require(_to != address(this), "Cannot transfer to the contract");
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(contractBalance >= _amount, "Insufficient Contract Balance");
 
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        payable(_to).transfer(_amount);
+        contractBalance -= _amount;
+
+        emit Transfer(_to, _amount);
     }
 }
